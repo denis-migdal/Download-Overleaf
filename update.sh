8cae482e60c5b3b7746595e68a7f52bcfd1f3f00
Denis Migdal <denis@migdal.ovh>
2021-04-07 16:38:24 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# JSON.parse( $("#data").text() ); // copy.
+cat data.json | jq -r '.projects | map([.id, .name] | join("\t") ) | join("\n")' > list.csv
+
+DATA=`cat list.csv`
+
+IFS="
+"
+
+mkdir -p ./all
+cd ./all
+
+
+function uriencode { jq -nr --arg v "$1" '$v|@uri'; }
+
+cred=false
+
+for i in $DATA ; do
+	id="${i%%	*}"
+	name="${i##*	}"
+
+	if [ -d "./$name" ]; then
+		git -C "./$name" remote update
+	else
+
+		if [ "$cred" = false ]; then
+			read -p "Username: " USERNAME
+			read -sp "Password: " PASSWORD
+			USERNAME=`uriencode "$USERNAME"`
+			PASSWORD=`uriencode "$PASSWORD"`
+			cred=true
+		fi
+
+		git clone --mirror --progress "https://$USERNAME:$PASSWORD@git.overleaf.com/$id" "$name"
+	fi
+done
+
+cd - > /dev/null
